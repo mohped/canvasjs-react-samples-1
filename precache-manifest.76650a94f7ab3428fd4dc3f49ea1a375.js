bf0fd5bc775840589c98d7f7c7ce16d21f156d77
mohammad Esmaeil Pedaran <essijnjn@gmail.com>
2020-11-04 11:23:02 +0330

    Updates

---
+++ precache-manifest.76650a94f7ab3428fd4dc3f49ea1a375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b5e644f956bc64b006f",
+    "url": "/canvasjs-react-samples-1/static/css/main.17f9e545.chunk.css"
+  },
+  {
+    "revision": "2b5e644f956bc64b006f",
+    "url": "/canvasjs-react-samples-1/static/js/main.2b5e644f.chunk.js"
+  },
+  {
+    "revision": "988665b1f9a017c3118e",
+    "url": "/canvasjs-react-samples-1/static/js/runtime~main.988665b1.js"
+  },
+  {
+    "revision": "a35ef37cfc7ac489aab3",
+    "url": "/canvasjs-react-samples-1/static/css/2.a25a1d11.chunk.css"
+  },
+  {
+    "revision": "a35ef37cfc7ac489aab3",
+    "url": "/canvasjs-react-samples-1/static/js/2.a35ef37c.chunk.js"
+  },
+  {
+    "revision": "cfea604ec4b48b76f176f4a1549a49a8",
+    "url": "/canvasjs-react-samples-1/index.html"
+  }
+];
