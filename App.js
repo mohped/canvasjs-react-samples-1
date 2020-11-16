a7aeaca1f9644f08ee9f69a352b21b3dc9a6b54e
mohammad Esmaeil Pedaran <essijnjn@gmail.com>
2020-11-16 23:16:55 +0330

    complete scatter

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Template from './views/Template';
+import $ from 'jquery';
+
+class App extends Component {
+    componentWillMount() {
+
+    }
+
+    render() {
+    return (
+
+		<Template/>
+    );
+  }
+}
+
+export default App;
